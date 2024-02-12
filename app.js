5c61dec1d187437ec1a228736e8fdceb74ba9d98
Ali Abodaraa <abodaraaali50@gmail.com>
2024-02-12 09:55:08 +0300

    send method of request

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 
-const http=require('http')
- const routes=require('./routes')    //the file content here acually cached by node can't added to it externaly you not do routes.ali property (you can only read from outside)
-
 const express=require('express')
-
 const app=express(); //create express app
+
 app.use((req,res,next)=>{
     console.log("In The First Middleware");
     next();//allow the request to continue to the next middleware in line
 })
 app.use((req,res,next)=>{
     console.log("In The Second Middleware");
-    res.send('<h1>Hello from Express !</h1>');//it make add content-type:text/html automatically
+    res.send('<h1>Hello from Express !</h1>');//it make add content-type:text/html automatically and stop do anything in any next middleware
 })
-const server = http.createServer(app)
-
-server.listen(3000,()=>{
-    console.log("Server Is Listening Now To 3000 Port")
-})
+// const server = http.createServer(app)
+// server.listen(3000,()=>{
+//     console.log("Server Is Listening Now To 3000 Port")
+// })
+//instead
+app.listen(3000)
