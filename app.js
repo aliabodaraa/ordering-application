b1632a4b54063ab968b5c8aa5ecd7fd926c918ed
Ali Abodaraa <abodaraaali50@gmail.com>
2024-02-13 17:37:30 +0300

    Add html using sendFile in response and use __dirname to determine the path to the view

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path=require('path');
 const bodyParser=require('body-parser');
 
 const app = express()
@@ -13,6 +13,6 @@ app.use("/admin",adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname,"views","404.html"));
 })
 app.listen(3000)
