98c618eae349ed7b714afd207a3e62482ff197d3
Ali Abodaraa <abodaraaali50@gmail.com>
2024-02-13 20:57:42 +0300

    serve statc files by add static middleware to express to tell it to forward any request to files that ends with 'css,js' extensions to 'public' folder allow request to access files system and add styles

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path=require('path');
 const bodyParser=require('body-parser');
+const rootPath = require('./utils/path');
 
 const app = express()
 
@@ -8,11 +9,11 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended:false}));
-
+app.use(express.static(path.join(rootPath,'public')));//express here will forward any request that tries to find '.css,.js' to public folder, allow some of them to access files system
 app.use("/admin",adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,"views","404.html"));
+    res.status(404).sendFile(path.join(rootPath,"views","404.html"));
 })
 app.listen(3000)
