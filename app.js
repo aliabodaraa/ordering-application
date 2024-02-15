358b4530e0a8ea764cf7ae3d48ca03deb7993413
Ali Abodaraa <abodaraaali50@gmail.com>
2024-02-15 20:24:23 +0300

    use ejs templating engine

--- app.js
+++ app.js
@@ -2,39 +2,23 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('hbs');
-const rootPath = require('./utils/path');
+const rootPath=require('./utils/path')
 
 const app = express();
 
-// app.engine(
-//   'hbs',
-//   expressHbs({
-//     layoutsDir: 'views/layouts/',
-//     defaultLayout: 'main-layout',
-//     extname: 'hbs'
-//   })
-// );
-// View Engine Setup 
-app.set('view engine', 'hbs') 
-app.set('view options', {//access to views
-    layout:'layouts/main-layout'
-})
-
-app.set('views','views') 
+app.set('view engine', 'ejs');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(rootPath,'public')));//express here will forward any request that tries to find '.css,.js' to public folder, allow some of them to access files system
-
+app.use(express.static(path.join(rootPath, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found',path:'' });
 });
 
 app.listen(3000);
