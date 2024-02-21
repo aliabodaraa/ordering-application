d86f5397c7d056597665ae2be651411a9f3dc63e
Ali Abodaraa <abodaraaali50@gmail.com>
2024-02-21 11:23:27 +0300

    Fetching Related Products for current loggedIn user

--- controllers/admin.js
+++ controllers/admin.js
@@ -28,7 +28,11 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId).then(product => {
+  //find products for current loggedin user
+  req.user.getProducts({where:{id:prodId}})
+  // Product.findByPk(prodId)
+  .then(products => {
+    let product=products[0];
     if (!product) {
       return res.redirect('/');
     }
@@ -60,7 +64,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then(products=>{
+  req.user.getProducts().then(products=>{
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
