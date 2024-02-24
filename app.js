1a4135e71c389ecedb209dd1a825c778af1be023
Ali Abodaraa <abodaraaali50@gmail.com>
2024-02-24 12:52:12 +0300

    connect to mongoDb

--- app.js
+++ app.js
@@ -5,73 +5,32 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
-const sequelize = require('./utils/database');
-
-//apply the relationships
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
-
 const app = express();
+
+const mongoConnect = require('./utils/database').mongoconnect;
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use((req,res,next)=>{
-    User.findByPk(1).then(user=>{
-        req.user=user;
-        next();
-    });
+    // User.findByPk(1).then(user=>{
+    //     req.user=user;
+    //     next();
+    // });
+    next();
 })
+
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-//relationships
-//1. Product - User
-Product.belongsTo(User,{constraints:true, onDelete:'CASCADE'});
-User.hasMany(Product);
-
-//2. User - Cart
-User.hasOne(Cart)
-Cart.belongsTo(User)
-
-//3. Cart - Product
-Cart.belongsToMany(Product, {through : CartItem})
-Product.belongsToMany(Cart, {through : CartItem})
-
-//3. Order - User
-Order.belongsTo(User)
-User.hasMany(Order);
-
-//3. Order - Product
-Order.belongsToMany(Product, {through : OrderItem}) //make you call `order.getProducts()` 
-Product.belongsToMany(Order, {through : OrderItem}) //make you call `product.getOrders()` 
-
-// sequelize.sync({force:true}).    //reconsidering the relationship that we newly setup
-
-sequelize.sync().
-then(result=>{
-    return User.findByPk(1)
-}).then(user=>{
-    if(!user)
-        return User.create({name:'Ali',email:'abodaraaali50@gmail.com'})
-    return user
-})
-.then(user=>{
-    // console.log(user);
-    return user.createCart();
-}).then(cart=>{
-    // console.log(cart);
+mongoConnect(()=>{
     app.listen(3000);
-}).catch(err=>console.log(err));  //look at all the models that you defined and then create tables for them
+})
--- utils/database.js
+++ utils/database.js
@@ -1,3 +1,21 @@
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize('node-complete','root','MasterR540',{dialect:'mysql',host:'localhost'});
-module.exports = sequelize;
+const {MongoClient}=require('mongodb');
+const ins_MongoClient=new MongoClient("mongodb://localhost:27017")
+let _db;
+const mongoconnect = (callback)=>{
+    ins_MongoClient.connect()
+    .then(client=>{
+        callback()
+        _db=client.db()
+        console.log("Connect___")
+    })
+    .catch(err=>console.log("err",err))
+}
+
+const getDB=()=>{
+    if(_db)
+        return _db;
+    throw "No Database Found"
+}
+
+exports.mongoconnect=mongoconnect;
+exports.getDB=getDB;
